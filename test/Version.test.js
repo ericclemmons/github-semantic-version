f786e7a9a1b9300caff4fe10682dec43246e2d8f
Eric Clemmons <eric@smarterspam.com>
2016-03-19 19:36:50 -0500

    Even dumber tests

---
+++ test/Version.test.js
@@ -0,0 +1,9 @@
+import expect from "expect";
+
+import Version from "../src/Version";
+
+describe("Version", function() {
+  it("should exist", function() {
+    expect(Version).toExist();
+  });
+});
